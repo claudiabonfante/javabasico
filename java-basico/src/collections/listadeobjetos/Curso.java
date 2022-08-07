9070c1463fdf4d5b22d0c3d51d766bbdf2b557b7
Claudia <claudiacbonfante@gmail.com>
2022-08-07 18:00:21 -0300

    Estudo sobre listas

--- java-basico/src/collections/listadeobjetos/Curso.java
+++ java-basico/src/collections/listadeobjetos/Curso.java
@@ -1,14 +1,13 @@
 package collections.listadeobjetos;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Curso {
     private String nome;
     private String instrutor;
 //    Usar arrayList quando for buscar posições de listas grandes e usar o LinkedList quando for sempre alterar a primeira posição
     private List<Aula> aulas = new LinkedList<Aula>();
+    private Set<Aluno> alunos = new HashSet<>();
 
     public Curso(String nome, String instrutor) {
         this.nome = nome;
@@ -30,5 +29,13 @@ public class Curso {
 
     public void adiciona(Aula aula){
         this.aulas.add(aula);
+    }
+
+    public void matricula(Aluno aluno) {
+        this.alunos.add(aluno);
+    }
+
+    public Set<Aluno> getAlunos() {
+        return Collections.unmodifiableSet(alunos);
     }
 }
---
+++ java-basico/src/collections/listadeobjetos/TesteCursoComAluno.java
@@ -0,0 +1,24 @@
+package collections.listadeobjetos;
+
+public class TesteCursoComAluno {
+    public static void main(String[] args) {
+
+        Curso javaCollections = new Curso("Curso 01", "Maria");
+        javaCollections.adiciona(new Aula("Aula 01", 5 ));
+        javaCollections.adiciona(new Aula("Aula 02", 15 ));
+        javaCollections.adiciona(new Aula("Aula 03", 25 ));
+
+        Aluno a1 = new Aluno("Primeiro Aluno", 1234);
+        Aluno a2 = new Aluno("Segundo Aluno", 5678);
+        Aluno a3 = new Aluno("Terceiro Aluno", 8901);
+
+        javaCollections.matricula(a1);
+        javaCollections.matricula(a2);
+        javaCollections.matricula(a3);
+
+        System.out.println("Todos alunos matriculados: ");
+        javaCollections.getAlunos().forEach(a->{
+            System.out.println(a);
+        });
+    }
+}
