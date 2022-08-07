9e56bb0bd739324d012c0203db52a2e34960a470
Claudia <claudiacbonfante@gmail.com>
2022-08-07 13:11:48 -0300

    Java basico collection

---
+++ java-basico/src/collections/TestandoListas.java
@@ -0,0 +1,43 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TestandoListas {
+    public static void main(String[] args) {
+        String teste1 = "Teste 01";
+        String teste2 = "Modelando testes";
+        String teste3 = "Trabalhando com testes";
+        String teste4 = "A - teste ordenacao ";
+
+        List<String> testes = new ArrayList<>();
+        testes.add(teste1);
+        testes.add(teste2);
+        testes.add(teste3);
+        testes.add(teste4);
+
+        System.out.println(testes);
+
+        testes.remove(0);
+
+        System.out.println(testes);
+
+        //para cada teste dentro de testes, faça:
+        for (String teste: testes) {
+            System.out.println("Teste: " + teste);
+        }
+
+        String primeiroTeste = testes.get(0);
+        System.out.println("Primeiro teste: " +primeiroTeste);
+
+        for (int i = 0; i < testes.size(); i++){
+            System.out.println("Teste: " + testes.get(i));
+        }
+
+        testes.forEach(teste -> {System.out.println("Percorrendo: "+teste);});
+
+        Collections.sort(testes);
+        System.out.println(testes);
+    }
+}
